c54445d39c7a429db6d69437929e42804e2375c6
rajuks <tech.ksraj541@gmail.com>
2026-02-10 23:54:22 -0800

    Fast API tutorial

---
+++ fastapi/backend/app/api/server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+def get_application():
+    app = FastAPI(title="fastapi_tutorial", version="1.0.0")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
+
+app = get_application()
